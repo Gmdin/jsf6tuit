c271b266fce1f23e937119e168f048b490153145
gmd <gmd@gamil.com>
2021-09-30 08:17:28 +0500

    'first'

--- js/index.js
+++ js/index.js
@@ -1,5 +1,40 @@
 console.log("this is first 6 tuit");
 //tuit2
-console.warn("this is warning");
-console.error("this is error");
-console.clear();
+// console.warn("this is warning");
+// console.error("this is error");
+// console.clear();
+
+//tuit 3
+// let num = 30;
+// console.log(num, typeof num);
+// let str = "30";
+// console.log(str, typeof str);
+// let bolean = true;
+// console.log(bolean, typeof bolean);
+// let undef;
+// console.log(undef, typeof undef);
+// let nu = null;
+// console.log(nu, typeof nu);
+// num = 32;
+// console.log(num, typeof num);
+
+// var hh = 333;
+// var hh = 22;
+// // const m;
+// const m = 56;
+// //m = 55;
+// console.log(m);
+//tuit 4
+//tuit 5
+let num = 30;
+num = String(30);
+console.log(num + typeof num);
+let boll = true;
+console.log(boll + typeof boll);
+boll = Number(true);
+console.log(boll + typeof boll);
+let num2 = 5;
+console.log(num + num2);
+let re;
+re = 50 + "true";
+console.log(re);
