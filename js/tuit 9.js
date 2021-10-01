7d9c96331b49acc06bfc52532d6609c9fc20b2ea
gmd <gmd@gamil.com>
2021-10-01 19:45:37 +0500

    10 tutorials

---
+++ js/tuit 9.js
@@ -0,0 +1,20 @@
+// console.log("tuit 9");
+// //loops /for/for in/while/do while
+// let ar = [2, 3, 4, 5, 6, 71, 2];
+// for (let i = 0; i < ar.length; i++) {
+//   console.log(ar[i]);
+// }
+// let obj = {
+//   name: "honey",
+//   age: 20,
+//   class: "software",
+// };
+// for (const i in obj) {
+//   console.log(i + obj[i]);
+// }
+// //while
+// // let i = 0;
+// // while (i <= ar.length) {
+// //   console.log(ar[i]);
+// //   i++;
+// // }
---
+++ js/tuit_10.js
@@ -0,0 +1,10 @@
+// console.log("tuit 10");
+// //functions in jS
+// function nam(name) {
+//   console.log(name);
+// }
+// nam("honey");
+// let add = function (a, b) {
+//   return a + b;
+// };
+// console.log(add(3, 8));
