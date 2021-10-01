09cb9f711c9a4f4ed07791fafe4559bc4c390463
gmd <gmd@gamil.com>
2021-10-01 21:33:39 +0500

    13ts tuitorials

---
+++ js/tuit_11.js
@@ -0,0 +1,10 @@
+console.log("tuit 11");
+//window object properties
+// let a = window.innerHeight;
+// let a = window.innerWidth;
+// let a = window.location;
+// alert("Location " + a);
+// let b = window.document;
+// let b = window.history;
+
+// console.log(b);
---
+++ js/tuit_12.js
@@ -0,0 +1,13 @@
+console.log("tuit 12");
+//UnderStanding DOM and creating DOM objects
+let a;
+// a = document.location;
+// a = document.all;
+// a = document.head;
+// a = document.body;
+// a = document.scripts;
+// a = document.title;
+// a = document.links;
+a = document.images;
+
+console.log(a);
