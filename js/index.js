445ca6ed62a3ab20079ac80c233f9f16d4b66413
gmd <gmd@gamil.com>
2021-10-01 17:49:59 +0500

    8 tutorials

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-console.log("this is first 6 tuit");
+//first comment
+console.log("this is first 5 tuit");
 //tuit2
 // console.warn("this is warning");
 // console.error("this is error");
@@ -26,15 +27,92 @@ console.log("this is first 6 tuit");
 // console.log(m);
 //tuit 4
 //tuit 5
-let num = 30;
-num = String(30);
-console.log(num + typeof num);
-let boll = true;
-console.log(boll + typeof boll);
-boll = Number(true);
-console.log(boll + typeof boll);
-let num2 = 5;
-console.log(num + num2);
-let re;
-re = 50 + "true";
-console.log(re);
+// let num = 30;
+// num = String(30);
+// console.log(num + typeof num);
+// let boll = true;
+// console.log(boll + typeof boll);
+// boll = Number(true);
+// console.log(boll + typeof boll);
+// let num2 = 5;
+// console.log(num + num2);
+// let re;
+// re = 50 + "true";
+// console.log(re);
+
+//tuit 6 methods and templete
+
+// let arr = [10, 20, 30, "asd", "40", 20];
+// let ch = "hy honey";
+// let bactek = `hy my name is ${ch} and arry is ${arr}`;
+//console.log(ch.charAt(4));
+// console.log(ch.slice(0, 4));
+// console.log(arr.indexOf(20));
+// console.log(ch.split(" "));
+// console.log(ch.toUpperCase());
+// console.log(ch.search("honey"));
+// console.log(ch.substr(1, 6));
+// console.log(arr.lastIndexOf(20));
+// console.log(bactek);
+// arr = arr.concat(ch);
+// console.log(arr);
+
+//tuit 7 Array and object
+
+// let arr = [10, 5, 15, 20, 3, 9];
+// // console.log(arr[5]);
+// // console.log(arr.length);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+// //console.log(arr.reverse());
+// // let arr1 = arr.sort(arr);
+// // console.log(arr1);
+// arr.push(500);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+// arr.pop();
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+// arr.shift();
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+// arr.unshift(35);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+// //Objects
+// let obj = {
+//   name: "honey",
+//   age: 20,
+//   subject: "JS",
+// };
+// console.log(obj.name);
+
+//Tuit 8 if/else and switch
+// 1st syntax
+// if(condition){
+//     if conditioin true tehn this portion run
+// }
+// else{
+//     otherwise this portion run
+// }
+//2nd syntax
+// (condition?true condition: false condition)
+// switches
+// switch(conditin){
+//     case 1:
+//         <code></code>
+//         break;
+//     case 2:
+//         <code></code>
+//         break;
+//     case 3:
+//         <code></code>
+//         break;
+//     default :
+//     <code></code>
+// }
